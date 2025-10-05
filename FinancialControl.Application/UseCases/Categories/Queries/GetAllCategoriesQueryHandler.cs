ef9de4d56519ca0b3ee4938b0bf8c7150b91f5d3
Douglas Modolo <douglasmodolo@outlook.com.br>
2025-10-05 16:35:28 -0300

    feat(categories): add endpoint to update category

--- FinancialControl.Application/UseCases/Categories/Queries/GetAllCategoriesQueryHandler.cs
+++ FinancialControl.Application/UseCases/Categories/Queries/GetAllCategoriesQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using FinancialControl.Application.ViewModels.Categories;
+﻿using FinancialControl.Application.Interfaces;
+using FinancialControl.Application.ViewModels.Categories;
 using FinancialControl.Domain.Entities;
 using FinancialControl.Domain.Repositories;
 using MediatR;
@@ -8,23 +9,29 @@ namespace FinancialControl.Application.UseCases.Categories.Queries
     public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, IEnumerable<CategoryViewModel>>
     {
         private readonly IGenericRepository<Category> _categoryRepository;
+        private readonly ILoggedUserService _loggedUserService;
 
-        public GetAllCategoriesQueryHandler(IGenericRepository<Category> categoryRepository)
+        public GetAllCategoriesQueryHandler(IGenericRepository<Category> categoryRepository, ILoggedUserService loggedUserService)
         {
             _categoryRepository = categoryRepository;
+            _loggedUserService = loggedUserService;
         }
 
         public async Task<IEnumerable<CategoryViewModel>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
         {
-            var categories = await _categoryRepository.GetAllAsync();
+            var userId = _loggedUserService.GetUserId();
 
-            var viewModels = categories.Select(c => new CategoryViewModel
-            {
-                Id = c.Id,
-                Name = c.Name
-            });
+            var allCategories = await _categoryRepository.GetAllAsync();
 
-            return viewModels;
+            var userCategories = allCategories
+                .Where(c => c.UserId == userId)
+                .Select(c => new CategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                });
+
+            return userCategories;
         }
     }
 }
