35c0bdfa6a81f27a35526950f4043d0e55ba4afd
Douglas Modolo <douglasmodolo@outlook.com.br>
2025-10-04 22:59:54 -0300

    feat(infra): implement ef core db context and generic repository

---
+++ FinancialControl.Infrastructure/Persistence/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using FinancialControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinancialControl.Infrastructure.Persistence.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title).IsRequired().HasMaxLength(100);
+
+            builder.Property(t => t.Amount).IsRequired().HasColumnType("decimal(18,2)");
+
+            builder.Property(t => t.Type).IsRequired();
+        }
+    }
+}
---
+++ FinancialControl.Infrastructure/Persistence/FinancialControlDbContext.cs
@@ -0,0 +1,24 @@
+﻿using FinancialControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FinancialControl.Infrastructure.Persistence
+{
+    public class FinancialControlDbContext : DbContext
+    {
+        public FinancialControlDbContext(DbContextOptions<FinancialControlDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
