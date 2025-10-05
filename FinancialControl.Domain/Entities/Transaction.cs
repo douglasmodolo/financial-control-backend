eccf4748be238b6184cc1680431864769e955842
Douglas Modolo <douglasmodolo@outlook.com.br>
2025-10-04 22:45:48 -0300

    feat(domain): create core entities and repository interfaces

---
+++ FinancialControl.Domain/Entities/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using FinancialControl.Domain.Common;
+using FinancialControl.Domain.Enums;
+
+namespace FinancialControl.Domain.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        public Transaction(string title, decimal amount, TransactionType type, Guid categoryId, Guid userId)
+        {
+            Title = title;
+            Amount = amount;
+            Type = type;
+            CategoryId = categoryId;
+            UserId = userId;
+        }
+
+        public string Title { get; private set; }
+        public decimal Amount { get; private set; }
+        public TransactionType Type { get; private set; }
+
+        public Guid CategoryId { get; private set; }
+        public Guid UserId { get; private set; }
+
+        public Category Category { get; private set; }
+        public User User { get; private set; }
+    }
+}
