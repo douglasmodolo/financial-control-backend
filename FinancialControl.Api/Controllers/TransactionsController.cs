a3b74c69f753ea1bc224671669efdadc39a94062
Douglas Modolo <douglasmodolo@outlook.com.br>
2025-10-05 13:56:07 -0300

    feat(api): add endpoint to create transactions

---
+++ FinancialControl.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,42 @@
+﻿using FinancialControl.Application.DTOs.Transactions;
+using FinancialControl.Application.UseCases.Transactions.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialControl.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TransactionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTransaction([FromBody] CreateTransactionDTO dto)
+        {
+            var command = new CreateTransactionCommand
+            {
+                Title = dto.Title,
+                Amount = dto.Amount,
+                Type = dto.Type,
+                CategoryId = dto.CategoryId,
+                UserId = dto.UserId
+            };
+
+            var transactionId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetTransactionById), new { id = transactionId }, new { id = transactionId });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTransactionById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
